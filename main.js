6e769fffede1fbcbed54c00758c591839346a48a
Elmosh <61812199+Elmosh32@users.noreply.github.com>
2022-04-14 21:08:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-  const notesContainer = document.getElementById("app");
+localStorage.clear();
+  
+const notesContainer = document.getElementById("app");
   const addNoteButton = notesContainer.querySelector(".add-note");
 
   getNotes().forEach((note) => {
